2fe0d4c25f06f012fce352e1064ca03fc9d310d2
Pavel Sanchez <pavel@bluedot.dev>
2021-01-20 22:01:17 -0500

    :sparkles: Added announcement banner that is session based

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,25 @@
 console.log('e11ty Connected');
 
 let lightMode = localStorage.getItem('lightMode');
+let announcement = sessionStorage.getItem('announcement');
+
 const body = document.querySelector('body');
 const sidebar = document.querySelector('#sidebar');
 const darkIcon = document.querySelector('.dm-icon');
 const menuIcon = document.querySelector('#menuIcon');
 const closeMenu = document.querySelector('#closeMenu');
+const closeAnnouncement = document.querySelector('#announcementClose');
+
+if (closeAnnouncement) {
+  closeAnnouncement.addEventListener('click', () => {
+    closeAnnouncement.style.display = 'none';
+    sessionStorage.setItem('announcement', null);
+  });
+}
+
+if (sessionStorage.getItem('announcement', null)) {
+  closeAnnouncement.style.display = 'none';
+}
 
 menuIcon.addEventListener('click', function () {
   console.log('clicked');
